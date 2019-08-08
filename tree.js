742f83cd5df2bdaf364adae3a0043ef6b7d62091
Viktoryia Valiuk <ackermanviktoria@gmail.com>
2019-08-08 20:33:28 +0200

    File uploaded

---
+++ tree.js
@@ -0,0 +1,62 @@
+class Tree {
+  constructor(key, meta, parent) {
+    this.parent = parent;
+    this.key = key;
+    this.meta = meta;
+    this.children = new Map();
+  }
+
+  getKey() {
+    return this.key;
+  }
+
+  getMeta() {
+    return this.meta;
+  }
+
+  addChild(key, meta) {
+    const child = new Tree(key, meta, this);
+    this.children.set(key, child);
+
+    return child;
+  }
+
+  getChild(key) {
+    return this.children.get(key);
+  }
+
+  // BEGIN (write your solution here)
+  hasChildren() {
+    for (let [key, value] of this.children) {
+      return true;
+    }
+    return false;
+  }
+
+  hasChild(key) {
+    return this.children.has(key);
+  }
+
+  getParent() {
+    return this.parent;
+  }
+  removeChild(key) {
+    return this.children.delete(key);
+  }
+  getDeepChild(keys) {
+   const result = keys.reduce((acc, key) => {
+     if (acc.hasChild(key)) {
+       return acc.getChild(key)
+     }
+     return this;
+   }, this);
+    return result === this ? undefined : result;
+  }
+
+  getChildren() {
+    return [...this.children.values()];
+  }
+  // END
+}
+
+export default Tree;
